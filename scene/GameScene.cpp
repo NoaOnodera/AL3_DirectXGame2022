f5bb2455dfbd9e044c0fd792424b0b2f149e9413
乃碧 小野寺 <deranoir102493@gmail.com>
2022-04-19 08:18:42 +0900

    追加

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,7 +6,10 @@ using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { 
+	//delete sprite_;
+	delete model_;
+}
 
 void GameScene::Initialize() {
 
@@ -14,9 +17,41 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+	textureHandle_ = TextureManager::Load("mario.jpg");//ファイル名を指定してテクスチャを読み込む
+	sprite_ = Sprite::Create(textureHandle_, {100, 0});//スプライトの生成
+	model_ = Model::Create();//3Dモデルの生成	
+	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
+	worldTransform_.rotation_ = {XM_PI / 4.0f, XM_PI / 4.0f, 0.0f};
+	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
+	worldTransform_.Initialize();//ワールドトランスフォームの初期化
+	viewProjection_.Initialize();//ビュープロジェクションの初期化
+
 }
 
-void GameScene::Update() {}
+void GameScene::Update() { 
+	//値を含んだ文字列
+	std::string translationDebug = std::string("translation:(") +
+	                       std::to_string(worldTransform_.translation_.x) + std::string(".")+
+	                       std::to_string(worldTransform_.translation_.y) + std::string(".")+
+	                       std::to_string(worldTransform_.translation_.z) + std::string(")");
+	debugText_->Print(translationDebug, 50, 50, 1.0f);
+
+
+	 std::string rotationDebug =
+	  std::string("rotation:(") + std::to_string(worldTransform_.rotation_.x) +
+	  std::string(".") + std::to_string(worldTransform_.rotation_.y) + std::string(".") +
+	  std::to_string(worldTransform_.rotation_.z) + std::string(")");
+	
+	debugText_->Print(rotationDebug, 50, 70, 1.0f);
+
+	std::string scaleDebug = std::string("scale:(") +
+	                            std::to_string(worldTransform_.scale_.x) + std::string(".") +
+	                            std::to_string(worldTransform_.scale_.y) + std::string(".") +
+	                            std::to_string(worldTransform_.scale_.z) + std::string(")");
+
+	debugText_->Print(scaleDebug, 50, 90, 1.0f);
+
+}
 
 void GameScene::Draw() {
 
@@ -30,7 +65,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
-
+	
 	// スプライト描画後処理
 	Sprite::PostDraw();
 	// 深度バッファクリア
@@ -44,6 +79,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	//3Dモデルの描画
+	model_->Draw(worldTransform_,viewProjection_,textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
@@ -56,12 +93,11 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);
 	//
 	// スプライト描画後処理
 	Sprite::PostDraw();
-
+	
 #pragma endregion
 }
