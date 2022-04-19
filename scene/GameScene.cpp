f4eaa0ce5530c80dbc8f50259a5e176e4af4b8a2
乃碧 小野寺 <deranoir102493@gmail.com>
2022-04-19 12:59:27 +0900

    8

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,13 +1,13 @@
 ﻿#include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
-#include<random>
+#include <random>
 using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() { 
-	//delete sprite_;
+GameScene::~GameScene() {
+	// delete sprite_;
 	delete model_;
 }
 
@@ -17,25 +17,28 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
-	textureHandle_ = TextureManager::Load("mario.jpg");//ファイル名を指定してテクスチャを読み込む
-	sprite_ = Sprite::Create(textureHandle_, {100, 0});//スプライトの生成
-	model_ = Model::Create();//3Dモデルの生成	
+	textureHandle_ = TextureManager::Load("mario.jpg"); //ファイル名を指定してテクスチャを読み込む
+	sprite_ = Sprite::Create(textureHandle_, {100, 0}); //スプライトの生成
+	model_ = Model::Create();                           // 3Dモデルの生成
 	std::random_device seed_gen;
 	std::mt19937_64 engine(seed_gen());
 	std::uniform_real_distribution<float> rotDist(0.0f, XM_2PI);
 	std::uniform_real_distribution<float> posDist(-10.0f, 10.0f);
 
-	for (size_t i= 0; i < _countof(worldTransform_); i++) {
+	for (size_t i = 0; i < _countof(worldTransform_); i++) {
 
-		worldTransform_[i].scale_ = {1.1f, 1.0f, 1.0f};
+		worldTransform_[i].scale_ = {1.0f, 1.0f, 1.0f};
 		worldTransform_[i].rotation_ = {rotDist(engine), rotDist(engine), rotDist(engine)};
 		worldTransform_[i].translation_ = {posDist(engine), posDist(engine), posDist(engine)};
 		worldTransform_[i].Initialize(); //ワールドトランスフォームの初期化
-		viewProjection_.Initialize(); //ビュープロジェクションの初期化
 	}
+	viewProjection_.up = {cosf(XM_PI / 4.0f), sin(XM_PI / 4.0f), 0.0f};
+	viewProjection_.target = {10, 0, 0};
+
+	viewProjection_.Initialize(); //ビュープロジェクションの初期化
 }
 
-void GameScene::Update() { 
+void GameScene::Update() {
 	//値を含んだ文字列
 	/*std::string translationDebug = std::string("translation:(") +
 	                       std::to_string(worldTransform_.translation_.x) + std::string(".")+
@@ -48,7 +51,7 @@ void GameScene::Update() {
 	  std::string("rotation:(") + std::to_string(worldTransform_.rotation_.x) +
 	  std::string(".") + std::to_string(worldTransform_.rotation_.y) + std::string(".") +
 	                            std::to_string(worldTransform_.rotation_.z) + std::string(")");
-	
+
 	debugText_->Print(rotationDebug, 50, 70, 1.0f);
 
 	std::string scaleDebug = std::string("scale:(") +
@@ -57,7 +60,57 @@ void GameScene::Update() {
 	                            std::to_string(worldTransform_.scale_.z) + std::string(")");
 
 	debugText_->Print(scaleDebug, 50, 90, 1.0f);*/
+	XMFLOAT3 move{0, 0, 0};
 
+	const float kEyeSpeed = 0.2f;
+
+	if (input_->PushKey(DIK_W)) {
+		move = {0, 0, kEyeSpeed};
+	} else if (input_->PushKey(DIK_S)) {
+		move = {0, 0, -kEyeSpeed};
+	}
+
+	viewProjection_.eye.x += move.x;
+	viewProjection_.eye.y += move.y;
+	viewProjection_.eye.z += move.z;
+
+	viewProjection_.UpdateMatrix();
+	debugText_->SetPos(50, 50);
+	debugText_->Printf(
+	  "eye:(%f,%f,%f)", viewProjection_.eye.x, viewProjection_.eye.y, viewProjection_.eye.z);
+
+	const float kTargetSpeed = 0.2f;
+
+	if (input_->PushKey(DIK_LEFT)) {
+		move = {-kTargetSpeed, 0, 0};
+	} else if (input_->PushKey(DIK_RIGHT)) {
+		move = {kTargetSpeed, 0, 0};
+	}
+
+	viewProjection_.target.x += move.x;
+	viewProjection_.target.y += move.y;
+	viewProjection_.target.z += move.z;
+
+	viewProjection_.UpdateMatrix();
+
+	debugText_->SetPos(50, 70);
+	debugText_->Printf(
+	  "target:(%f,%f,%f)", viewProjection_.target.x, viewProjection_.target.y,
+	  viewProjection_.target.z);
+	const float kUpRotSpeed = 0.05f;
+
+	if (input_->PushKey(DIK_SPACE)) {
+		viewAngle += kUpRotSpeed;
+
+		viewAngle = fmodf(viewAngle, XM_2PI);
+	}
+
+	viewProjection_.up = {cosf(viewAngle), sinf(viewAngle), 0.0f};
+	viewProjection_.UpdateMatrix();
+
+	debugText_->SetPos(50, 90);
+	debugText_->Printf(
+	  "up:(% f, % f,% f) ", viewProjection_.up.x, viewProjection_.up.y, viewProjection_.up.z);
 }
 
 void GameScene::Draw() {
@@ -72,7 +125,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
-	
+
 	// スプライト描画後処理
 	Sprite::PostDraw();
 	// 深度バッファクリア
@@ -86,7 +139,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-	//3Dモデルの描画
+	// 3Dモデルの描画
 	for (size_t i = 0; i < _countof(worldTransform_); i++) {
 
 		model_->Draw(worldTransform_[i], viewProjection_, textureHandle_);
@@ -107,6 +160,6 @@ void GameScene::Draw() {
 	//
 	// スプライト描画後処理
 	Sprite::PostDraw();
-	
+
 #pragma endregion
 }
